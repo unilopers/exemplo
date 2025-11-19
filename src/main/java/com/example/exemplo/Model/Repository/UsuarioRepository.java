ac179d06a8f479450c89818cc016cda32adeb057
Marcos "Marcão" Aurelio <macosaures@gmail.com>
2025-11-18 21:01:35 -0300

    ✨ Add UsuarioRepository interface for user data access

---
+++ src/main/java/com/example/exemplo/Model/Repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.exemplo.Model.Repository;
+
+import com.example.exemplo.Model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
